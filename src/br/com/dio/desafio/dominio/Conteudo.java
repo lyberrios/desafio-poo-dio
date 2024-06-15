b097fc2551eff3128eb46ac54d382d02af9c3947
Ermarly Berrios <lyberryo@gmail.com>
2024-06-14 21:25:57 -0300

    Finalizar aplicacion

---
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -0,0 +1,28 @@
+package br.com.dio.desafio.dominio;
+
+public abstract class Conteudo {
+    private String titulo;
+    private String descricao;
+    private int cargahoraria; // Este es el atributo cargahoraria
+
+    protected static final double XP_PADRAO = 10;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+    public abstract double calcularXp();
+}
